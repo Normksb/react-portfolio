9df734cde783f4d4476596635510e0ee0be1e581
Norman Bernard <nksb414@gmail.com>
2021-12-15 15:42:39 +0800

    added about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from './components/Header';
+import About from './pages/About'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
 
     return (
         <div className='App'>
             <Header/>
+            <Switch>
+                <Route exact path='/'>
+                    <About/>
+                </Route>
+            </Switch>
         </div>
     )
 }
---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+const About = () => {
+    return (
+        <div id="about">
+        <div class="about-content">
+
+        
+        <h1 class="content-item">
+            Norman Bernard
+        </h1>
+
+        <img src="./images/20211203_104329.jpg" alt="Norman Bernard" class="content-item nb-image"/>
+
+        <h2 class="content-item">
+            Fullstack Web Developer
+        </h2>
+
+        <p class="content-item">
+
+            Fullstack web developer with nearly 20 years of experience in tech support and a variety of support roles within software development-oriented businesses. Recently completed a certificate in Fullstack Web Development with the University of Western Australia.
+
+        </p>
+
+        <a href="/portfolio.html" class="btn">See Portfolio</a>
+        </div>
+    </div>
+    ) 
+
+}
+
+export default About;
